664d508db5a6a873bd07dedf62d626dfa8bc6770
kidoyoshiyuki <kido@test>
2023-07-05 19:40:39 +0900

    test8

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,6 +23,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
+                echo "*******************************" . route('/');
                 return redirect(RouteServiceProvider::HOME);
             }
         }
