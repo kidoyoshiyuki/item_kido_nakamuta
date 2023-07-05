672454822c518579b6f710f260f242c1c287a1fb
kidoyoshiyuki <kido@test>
2023-07-05 18:52:00 +0900

    test4

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            dd(route('login'));
+            echo route('login');
             return route('login');
         }
     }
